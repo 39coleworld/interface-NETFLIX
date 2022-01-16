af2ac6052f546e71cc5016676d3fd52e34d2b575
39coleworld <vinicius.billabong_95@hotmail.com>
2022-01-16 14:34:26 -0300

    the first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+
